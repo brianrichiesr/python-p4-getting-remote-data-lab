ba7fe74e723a70cb1290ea7c3aa7ba142098dece
Brian Richie Sr <brianrichiesr@gmail.com>
2023-11-18 17:46:36 -0800

    complete lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,13 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        items = json.loads(self.get_response_body())
+        item_list = []
+        for item in items:
+            item_list.append(item)
+        
+        return item_list
